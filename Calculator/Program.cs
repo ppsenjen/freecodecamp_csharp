d981e89414105859c8424f83aca9412eb4217587
Pam Psenjen <ppsenjen@fameve.com>
2024-06-28 00:05:14 +0300

    adding two numbers using methods

---
+++ Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace calculator
+{
+    //trying out a basic calculator using methods with parametres
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firsnum = ReadInt("Enter first number");
+            int secondnum = ReadInt("Enter second number");
+            int result = Add(firsnum, secondnum);
+        }
+
+        static int ReadInt(string message)
+        {
+            Console.WriteLine($"{message}");
+            return Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
