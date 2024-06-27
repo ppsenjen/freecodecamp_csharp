a941339210acd2edf4ae586c1d2eb1ffddc4d713
Pam Psenjen <ppsenjen@fameve.com>
2024-06-28 00:11:06 +0300

    using try catch

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exception_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+
+            }
+            catch (Exception e)
+            {
+
+
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+
+        }
+    }
+}
