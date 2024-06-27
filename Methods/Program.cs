30b8dd81f3ecaf2777a649a73844289e04c3c3a7
Pam Psenjen <ppsenjen@fameve.com>
2024-06-28 00:19:11 +0300

    using methods

---
+++ Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+            PrintIntroduction();
+
+            Console.Title = ReturnName();
+
+            Console.ReadLine();
+
+        }
+
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Hello, World Pam!");
+
+
+        }
+
+        static void PrintIntroduction()
+        {
+            Console.WriteLine("pam");
+
+        }
+        static string ReturnName()
+        {
+
+            return "pam";
+
+        }
+    }
+}
