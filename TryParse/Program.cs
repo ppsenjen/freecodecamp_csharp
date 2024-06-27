b1f3f6d97d7f136e35201830da655458f5144a38
Pam Psenjen <ppsenjen@fameve.com>
2024-06-27 18:23:03 +0300

    Add project files.

---
+++ TryParse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Channels;
+
+namespace try_catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool looping = true;
+
+            while (looping)
+                try
+                {
+                    Console.WriteLine("Enter a number");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(num);
+
+                    looping = false;
+                }
+
+                //Adding the e keyword to return a more specific error message
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+
+            Console.WriteLine("Goodbye!");
+            Console.ReadLine();
+        }
+
+    }
+}
+
