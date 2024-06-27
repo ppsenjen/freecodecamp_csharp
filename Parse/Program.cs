78201c8a1aba76a0b1424d0d804b64b0dc708dd6
Pam Psenjen <ppsenjen@fameve.com>
2024-06-28 00:30:26 +0300

    using IntTryParse

---
+++ Parse/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace Parse
+{
+    class MyClass1
+    {
+
+        static void Main(string[] args)
+        {
+            /* using int 
+             
+            string input = "123";
+            int result;
+
+            bool success = int.TryParse(input, out result);
+
+            if(success )
+            {
+                Console.WriteLine($"Conversation successful: {result}");
+            }
+            else
+                Console.WriteLine("Conversation failed");
+            
+
+            // using double 
+
+            string input = "123.45";
+            double result;
+
+            bool success = double.TryParse(input, out result );
+
+            if ( success ) // (double.TryParse(input, out double result))
+
+
+            {
+                Console.WriteLine($"Conversion successfull {result}");
+            }
+
+            else
+            {
+                Console.WriteLine("failed");
+
+            }
+
+            */
+
+
+            while (true)
+            {
+
+                Console.WriteLine("Enter your age");
+                string input = Console.ReadLine();
+
+
+                if (int.TryParse(input, out int age))
+                {
+                    Console.WriteLine($"You entered a valid age:  {age}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid age please enter a valid age ");
+                }
+
+            }
+
+        }
+
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
