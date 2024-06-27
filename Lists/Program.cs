3debca630775a203a23de90e39afc3348377af42
Pam Psenjen <ppsenjen@fameve.com>
2024-06-27 18:38:42 +0300

    added lists

---
+++ Lists/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //with lists you can add numbers 
+            List<int> listNumbers = new List<int>()
+            { 
+                //with lists you can add numbers 
+                //arrays when you add numbers you will get an error 
+            1, 2, 3, 4, 5
+
+            };
+
+            //promting the user to enter a numbers
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                //listNumbers.Add(num);
+            }
+
+            //printing numbers to screen
+            Console.WriteLine();
+            foreach (var item in listNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
