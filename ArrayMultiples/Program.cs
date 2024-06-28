7c930bfc105406da34a4cdbbe97daee6e24469bf
Pam Psenjen <ppsenjen@fameve.com>
2024-06-28 16:50:22 +0300

    structure vs class

--- ArrayMultiples/Program.cs
+++ ArrayMultiples/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace ArrayMultiples
+﻿using System.Diagnostics.Metrics;
+
+namespace Arraymultiples
 {
     internal class Program
     {
+        /*
+         * Define and initialize two intergers (num, length)
+         * (7,5) -> [7, 14, 21, 28, 35]
+         * Create int array with size length
+         * loop thru and insert the ( loop counter x num) into the array
+         * print the final array
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num = 10;
+            int length = 10;
+            int counter = 0;
+
+            int[] result = new int[length];
+
+            for (int i = 1; i < result.Length; i++)
+            {
+                result[counter] = num * i;
+                counter++;
+
+            }
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item}");
+
+            }
+
         }
+
+
     }
 }
---
+++ Class/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Class
+{
+    internal class Program
+    {
+        //difference between a structure and a class 
+        /*
+         * A structure is limited to one constructor since all of the fields
+         * needs to be assyned once the struct is created
+         * A structure you have to declare all the variables inside the parametre and assign them all inside the constructor
+         * 
+         * A class can have many constructors even an empty contrutor is accepted
+         * 
+         */
+
+        class Person 
+        {
+            public string name;
+            public int age;
+
+            public Person() 
+            { 
+            }
+
+            public Person (string name)
+            {
+                this.name = name;
+                this.age = 0;
+            }
+
+            public Person (int age)
+            {
+                this.age = age;
+                this.name = "";
+            }
+
+            public Person (string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+        
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Person person = new Person(name, age);
+
+            Console.WriteLine(person.name);
+            Console.WriteLine(person.age);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
