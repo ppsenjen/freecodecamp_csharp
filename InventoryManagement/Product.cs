31bbc296518a720888171cefb7fee6154bae8d72
Pam Psenjen <ppsenjen@fameve.com>
2024-07-03 08:46:40 +0300

    inventory management of a pie shop

---
+++ InventoryManagement/Product.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class Product
+    {
+
+      /* the string? in description field means that it can be left empty
+      string.Empty = the name can be empty
+      */  
+private int id;
+private string name = string.Empty;
+private string? description;
+private int maxItemsInStock = 0;
+private int amountInStock = 0;
+private bool isBelowStockThreshold = false;
+
+
+public void UseProduct(int items)
+{
+if 
+
+
+}
+
+    }
+
+
+}
