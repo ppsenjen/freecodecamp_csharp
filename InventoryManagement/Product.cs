fe6bdabf4168462ddb3490c0329d5621fd210a1c
Pam Psenjen <ppsenjen@fameve.com>
2024-07-03 10:30:48 +0300

    implemeting the Product class

--- InventoryManagement/Product.cs
+++ InventoryManagement/Product.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,12 +23,63 @@ private bool isBelowStockThreshold = false;
 
 public void UseProduct(int items)
 {
-if 
 
+if (items <= amountInStock)
+{
+//use the items
+amountInStock-=items;
+
+UpdateLowStock();
+Log($"Amount in stock updated. Now {amountInStock} items in stock");
+
+}
+
+else 
+{
+Log($"Not enough items on stock for{CreateSimpleProductRepresentation()}. {amountInStock} available but {items} requested");
+}
+}
+
+
+public void IncreaseStock()
+{
+amountInStock++;
+
+}
+
+private void DecreaseStock(int items, string reason)
+{
+if (items <= amountInStock)
+{
+    //decreasing the stock
+}
+
+}
+
+private string CreateSimpleProductRepresentation()
+{
+    return $"Product {id} ({name})";
+}
+
+private void Log(string message)
+{
+Console.WriteLine(message);
+
+}
+
+
+private void UpdateLowStock()
+{
+if (amountInStock <= 10) // for now 10 is fixed
+{
+isBelowStockThreshold = true;
+
+}
+
+}
 
 }
 
     }
 
 
-}
